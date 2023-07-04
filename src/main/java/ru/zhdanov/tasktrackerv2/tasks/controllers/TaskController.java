4815bc986d2df76717874ff186fe66dce466188b
stinkos <zdnn182@hotmail.com>
2023-07-04 13:46:12 +0500

    get userId from Principal

--- src/main/java/ru/zhdanov/tasktrackerv2/tasks/controllers/TaskController.java
+++ src/main/java/ru/zhdanov/tasktrackerv2/tasks/controllers/TaskController.java
@@ -2,7 +2,9 @@ package ru.zhdanov.tasktrackerv2.tasks.controllers;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
+import ru.zhdanov.tasktrackerv2.auth.security.UserDetailsImpl;
 import ru.zhdanov.tasktrackerv2.tasks.dto.TaskDto;
 import ru.zhdanov.tasktrackerv2.tasks.entity.Task;
 import ru.zhdanov.tasktrackerv2.tasks.mappers.TaskMapper;
@@ -11,7 +13,7 @@ import ru.zhdanov.tasktrackerv2.tasks.services.TaskService;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/task")
+@RequestMapping("/api/v1/")
 @RequiredArgsConstructor
 public class TaskController {
 
@@ -20,37 +22,38 @@ public class TaskController {
     private final TaskMapper taskMapper;
 
 
-
-    @GetMapping("/{id}")
-    public TaskDto getTaskById(@PathVariable int id){
+    @GetMapping("/task/{id}")
+    public TaskDto getTaskById(@PathVariable int id) {
         Task task = taskService.get(id);
 
         return taskMapper.convertToTaskDto(task);
     }
 
-    @GetMapping("/{id}/tasks")
-    public List<TaskDto> getAllTaskByUserId(@PathVariable int id){
-        List<Task> taskList = taskService.findAllByUserId(id);
+    @GetMapping("/tasks")
+    public List<TaskDto> getAllTaskByUserId() {
+        UserDetailsImpl userDetails = (UserDetailsImpl) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        List<Task> taskList = taskService.findAllByUserId(userDetails.getUser().getId());
 
         return taskList.stream().map(taskMapper::convertToTaskDto).toList();
     }
 
     @PatchMapping("/{id}")
-    public TaskDto update(@PathVariable int id ,@RequestBody @Valid TaskDto taskDto){
-        Task updatedTask = taskService.save(id,taskMapper.convertToTask(taskDto));
+    public TaskDto update(@PathVariable int id, @RequestBody @Valid TaskDto taskDto) {
+        Task updatedTask = taskService.save(id, taskMapper.convertToTask(taskDto));
 
         return taskMapper.convertToTaskDto(updatedTask);
     }
 
     @DeleteMapping("/{id}")
-    public void delete(@PathVariable int id){
+    public void delete(@PathVariable int id) {
         taskService.deleteById(id);
     }
 
-    @PostMapping("/{id}/tasks")
-    public TaskDto createTask(@PathVariable int id,@RequestBody @Valid TaskDto taskDto){
+    @PostMapping("/task")
+    public TaskDto createTask(@RequestBody @Valid TaskDto taskDto) {
         Task task = taskMapper.convertToTask(taskDto);
-        Task createdTask = taskService.saveWithUserId(task,id);
+        UserDetailsImpl userDetails = (UserDetailsImpl) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        Task createdTask = taskService.saveWithUserId(task, userDetails.getUser().getId());
         return taskMapper.convertToTaskDto(createdTask);
     }
 }
